27d5cf60a40cb3fdd35270c621094a59d08d7c64
Laith <Pikachu.jo02@gmail.com>
2024-03-12 12:46:29 +0300

    Add project files.

---
+++ Dynasty8.DataAccess/Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynasty8.DataAccess.Repository
+{
+    public class Repository
+    {
+    }
+}
---
+++ Dynasty8.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dynasty8.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
